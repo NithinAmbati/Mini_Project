0bcc74732e7f7d304e1f67a2aa49379d8a6899a9
NithinAmbati <nithinambati9@gmail.com>
2024-07-10 10:16:59 +0530

    Done

--- backend/APIs/verifyProfile.js
+++ backend/APIs/verifyProfile.js
@@ -3,15 +3,19 @@ const { Student } = require("./mongoDBConnection");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const authorization = req.header["authorization"];
-  if (!authorization) res.status(403).send("Invalid authorization");
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) {
+    res.status(401).send("Invalid Access Token");
+    return;
+  }
   const jwtToken = authHeader.split(" ")[1];
   if (!jwtToken) res.status(403).send("Invalid JWT token");
   try {
-    const payload = jwt.verify(jwtToken, "your_secret_key");
+    const payload = jwt.verify(jwtToken, "Nithin");
     const { email } = payload;
     const user = await Student.find({ email });
     console.log(user);
+    res.status(200).send("true");
   } catch (error) {
     res.status(403).send(error.message);
   }
